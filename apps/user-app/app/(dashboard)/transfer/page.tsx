3072d8a44c905f78e9d81881d29c11e705fa3f36
pankaj kumar <Pankajams1234@gmail.com>
2024-11-11 00:32:23 +0530

    do added more compents and little transfer page

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>Transfer</div>
-  )
+import prisma from "@repo/db/client";
+import { getSession } from "next-auth/react";
+import { AddMoney } from "../../../components/AddMoneyCard"
+async function getBalance() {
+  const session = await getSession();
+  const balance = await prisma.balance.findFirst({
+    where: {
+      userId: Number(session?.user?.id),
+    },
+  });
+  return <div></div>;
 }
-
-export default page
