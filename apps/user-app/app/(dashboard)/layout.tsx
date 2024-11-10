d755f3f64f78144e51650e5dee36de7dd8f12f8e
pankaj kumar <Pankajams1234@gmail.com>
2024-11-11 00:05:43 +0530

    added some componets

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -24,6 +24,7 @@ export default function Layout({
           />
         </div>
       </div>
+      {children}
     </div>
   );
 }
--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,4 +1,4 @@
-export function card({
+export function Card({
   title,
   children,
 }: {
