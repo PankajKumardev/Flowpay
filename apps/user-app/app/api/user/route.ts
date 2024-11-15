5fa8d45fefff2b1fce3abfeffd58057a8a179412
pankaj kumar <pankajamsali@gmail.com>
2024-11-16 00:17:24 +0530

    feat: fix user session retrieval and enhance P2P transfer transaction handling

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -7,7 +7,7 @@ export const GET = async () => {
         const session = await getServerSession(authOptions);
         if (session.user) {
             return NextResponse.json({
-                user:   .user
+                user : session.user
             })
         }
     }
