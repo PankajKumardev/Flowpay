f929a7b15bbb604e740286677d4090a966f852af
pankaj kumar <pankajamsali@gmail.com>
2024-11-15 23:34:15 +0530

    feat: implement P2P transfer functionality with SendCard component and update P2P page layout

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -1,6 +1,12 @@
+import { SendCard } from "../../../components/SendCard";
+
 export default function () {
-    return <div 
-    className="mt-20"
-    >P2P</div>;
-  }
-  
+  return (
+    <div className="w-full">
+      <div className="flex justify-center text-3xl mt-20 font-extrabold pr-4 text-indigo-600">
+        P2P Transfer
+      </div>
+      <SendCard />
+    </div>
+  );
+}
