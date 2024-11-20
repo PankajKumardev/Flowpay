70c107823c6c5ef24500abb018879b1eab9fe3da
pankaj kumar <pankajamsali@gmail.com>
2024-11-20 11:19:07 +0530

    fix: streamline session retrieval and improve formatting in transfer page

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -6,7 +6,7 @@ import { BalanceCard } from "../../../components/BalanceCard";
 import { OnRampTransaction } from "../../../components/OnRampTransaction";
 
 async function getBalance() {
-  const session = await getServerSession(await authOptions);
+  const session = await getServerSession(authOptions);
   const balance = await prisma.balance.findFirst({
     where: {
       userId: Number(session?.user?.id),
@@ -25,7 +25,7 @@ async function getOnRampTransactions() {
       userId: Number(session?.user.id),
     },
   });
-  return txns.map((t : any) => ({
+  return txns.map((t: any) => ({
     time: t.startTime,
     amount: t.amount,
     status: t.status,
@@ -39,21 +39,21 @@ export default async function () {
   return (
     <div className="w-full mt-4 ">
       <div className="text-4xl pt-8 mb-8 font-bold text-violet-600 flex justify-center ">
-      <h1 className="text-4xl font-extrabold text-slate-800">
-        <span className="text-blue-600">FlowPay </span>Transfer
-      </h1>
+        <h1 className="text-4xl font-extrabold text-slate-800">
+          <span className="text-blue-600">FlowPay </span>Transfer
+        </h1>
       </div>
       <div className=" gap-4 md:grid-cols-2 pt-4  md:px-28">
         <div>
           <AddMoney />
         </div>
         <div>
-         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pt-4 px-2">
-         <BalanceCard amount={balance.amount} locked={balance.locked}  />
-          <div>
-            <OnRampTransaction transactions={transactions} />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pt-4 px-2">
+            <BalanceCard amount={balance.amount} locked={balance.locked} />
+            <div>
+              <OnRampTransaction transactions={transactions} />
+            </div>
           </div>
-         </div>
         </div>
       </div>
     </div>
