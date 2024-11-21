c744b442d08b24f2902ec255f3952b530af2aa89
pankaj kumar <pankajamsali@gmail.com>
2024-11-21 19:49:48 +0530

    feat: add Bank page with HDFC transaction component integration

---
+++ apps/user-app/app/(dashboard)/Bank/page.tsx
@@ -0,0 +1,10 @@
+import AxisTransactionPage from "../../../components/AxisBank";
+import HDFCTransactionPage from "../../../components/HdfcBank";
+
+export default function Bank() {
+    return (
+        <div className="pt-20">
+         <HDFCTransactionPage/> 
+        </div>
+        );
+}
