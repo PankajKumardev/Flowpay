e3f1ad3ebc5931004051045c5c63f585a9fbe0ae
pankaj kumar <pankajamsali@gmail.com>
2024-12-06 11:49:22 +0530

    Add SpeedInsights component and update package.json dependencies

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,15 +1,16 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Providers } from "../provider";
-import ClientWrapper from "../ClientWrapper";
-import { Analytics } from "@vercel/analytics/react";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Providers } from '../provider';
+import ClientWrapper from '../ClientWrapper';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Wallet",
-  description: "Simple wallet app",
+  title: 'Wallet',
+  description: 'Simple wallet app',
 };
 
 export default function RootLayout({
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <ClientWrapper>
+            <SpeedInsights />
             <Analytics />
             {children}
           </ClientWrapper>
